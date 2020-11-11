02b5ef55b6989c4c8d6c71c243e48edc62b86338
Ekaterina <Kata_Yurevna@mail.ru>
2020-11-11 22:57:02 +0700

    обновление проекта

--- build/js/script.js
+++ build/js/script.js
@@ -1,20 +1,52 @@
 'use strict';
 
 (function () {
-  var body = document.querySelector('body');
-  var header = document.querySelector('.header');
-  var toggler = document.querySelector('.toggle');
-  var navigation = document.querySelector('.header__list');
+  var anchors = document.querySelectorAll('a[href*="#"]');
 
-  body.classList.remove('no-js');
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (evt) {
+      evt.preventDefault()
 
-  toggler.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    navigation.classList.toggle('header__list--show');
-    header.classList.toggle('header__show');
-    toggler.classList.toggle('toggle--menu');
-    toggler.classList.toggle('toggle--cross');
-  });
-})();
+      const blockID = anchor.getAttribute('href').substr(1)
 
-svg4everybody();
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
+
+  var inputTelPopup = document.querySelector('.popup__input--tel');
+  var inputTelForm = document.querySelector('.form__input--tel');
+
+  function setMaskTel(input) {
+    input.onfocus = function () {
+      input.value = '+7(';
+    };
+    var old = 0;
+    input.onkeydown = function () {
+      var curLen = input.value.length;
+      if (curLen < old) {
+        old--;
+        return;
+      }
+      if (curLen === 6) {
+        input.value = input.value + ')-';
+      }
+      if (curLen === 11) {
+        input.value = input.value + '-';
+      }
+      if (curLen === 14) {
+        input.value = input.value + '-';
+      }
+      if (curLen > 16) {
+        input.value = input.value.substring(0, input.value.length - 1);
+      }
+      old++;
+    };
+  }
+
+ setMaskTel(inputTelForm);
+  setMaskTel(inputTelPopup);
+  svg4everybody();
+}) ();
